6dee8969a800755c9d6a3fee3c08e4d332c31414
Jonathan Strickland <djanatyn@gmail.com>
2013-06-26 12:00:31 -0400

    adjust animation speed and add sample animation

--- out/convert.sh
+++ out/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ls -1tr gen*.png | xargs -i convert -scale 800% {} {}
-convert -delay 5 -loop 0 $(ls -1tr gen*.png) animation.gif
+convert -delay 3 -loop 0 $(ls -1tr gen*.png) animation.gif
