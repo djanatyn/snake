7404c5732eec5e81922b621e722e6423903f3dfa
Jonathan Strickland <djanatyn@gmail.com>
2013-06-26 10:01:16 -0400

    updated project.clj with more info and location of -main function

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject snake "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a snake simulation with AI"
+  :url "http://github.com/djanatyn/snake"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main snake.render)
