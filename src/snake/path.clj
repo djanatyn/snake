cf8c8542e6ecd7fca1a999ead3a47f877b0c6c4d
Jonathan Strickland <djanatyn@gmail.com>
2013-06-27 11:14:47 -0400

    more...pathing stuff?

--- src/snake/path.clj
+++ src/snake/path.clj
@@ -1,27 +1,33 @@
 (ns snake.path
   (:use snake.grid snake.core))
 
-
+(defn neighbors
+  "return the coordinates of all adjacent tiles"
+  [grid [x y]]
+  (let [delta-spaces [[0 1] [-1 0] [1 0] [0 -1]]
+        surroundings (map #(map + [x y] %) delta-spaces)]
+    (filter (partial valid? grid) surroundings)))
 
 (defn distance [[x1 y1] [x2 y2]]
   (Math/sqrt (+ (Math/pow (- x2 x1) 2) (Math/pow (- y2 y1) 2))))
 
 (defn start-path [world]
-  (map (fn [cell] {:path [cell] :score (distance (:food world) cell)}) (neighbors world (:head world))))
+  (set (map (fn [cell] {:path [cell] :score (distance (:food world) cell)}) (neighbors world (:head world)))))
 
 (defn extend-path [path world]
-  (map (fn [cell] {:path (cons cell (:path path)) :score (distance (:food world) cell)}) (neighbors world (first (:path path)))))
+  (let [new-spaces (neighbors world (first (:path path)))]
+    (if (not= (:score path) 0)
+      (if (empty? new-spaces) nil
+        (map (fn [cell] {:path (cons cell (:path path)) :score (distance (:food world) cell)}) (neighbors world (first (:path path)))))
+      path)))
 
 (defn current-best [paths]
   (apply (partial min-key :score) paths))
 
-(defn step [world steps]
-  (take steps (iterate #(current-best (extend-path % world)) (current-best (start-path world)))))
+(defn improve-paths [paths world]
+  (let [best (current-best paths)
+        new-paths (extend-path best world)]
+    (flatten (conj (remove (partial = best) paths) new-paths))))
 
-(defn neighbors
-  "return the coordinates of all adjacent tiles"
-  [grid [x y]]
-  (let [delta-spaces [[-1 1] [0 1] [1 1] [-1 0] [1 0] [-1 -1] [0 -1] [1 -1]]
-        surroundings (map #(map + [x y] %) delta-spaces)]
-    (filter (partial valid? grid) surroundings)))
-
+(defn walk-down-paths [world n]
+  (last (take n (iterate #(improve-paths % world) (start-path world)))))
